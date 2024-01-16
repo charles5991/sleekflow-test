6a1f6ce63d3998c5ad8af0cfd9b5d548edf27b17
charles5991 <charleschan5991@gmail.com>
2024-01-17 04:45:08 +0800

    update

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,16 @@
-var tailwindcss = require('tailwindcss');
-const purgecss = require('@fullhuman/postcss-purgecss')({
+var tailwindcss = require("tailwindcss")
+const purgecss = require("@fullhuman/postcss-purgecss")({
   // Specify the paths to all of the template files in your project
   content: [
-    './src/**/*.html',
-    './src/**/*.js',
-    './src/**/*.jsx',
+    "./src/**/*.html",
+    "./src/**/*.js",
+    "./src/**/*.jsx"
     // etc.
   ],
 
   // Include any special characters you're using in this regular expression
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-});
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+})
 module.exports = {
-  plugins: [
-    tailwindcss('./src/tailwind.config.js'),
-    require('autoprefixer'),
-    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
-  ],
-};
+  plugins: [tailwindcss("./src/tailwind.config.js"), require("autoprefixer")]
+}
